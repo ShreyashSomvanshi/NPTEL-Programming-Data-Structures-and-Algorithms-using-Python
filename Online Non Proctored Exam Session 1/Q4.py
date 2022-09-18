5c4d3267ed5fe01bac951c8f4e6bf55a6180403c
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-09-18 23:21:31 +0530

    Create Q4.py
    NPTEL-Programming, Data Structures and Algorithms using Python 
    Non Proctored Online Exam - Question 4

---
+++ Online Non Proctored Exam Session 1/Q4.py
@@ -0,0 +1,26 @@
+'''
+A list is a decreasing if each element is strictly smaller than the preceding one.
+For instance [], [7], [11,8] and [89,63,44,19,3] are decreasing, while [3,18,4] and [23,14,14,3] are not.
+Here is a recursive function to check if a list is decreasing.
+You have to fill in the missing argument for the recursive call.
+
+def decreasing(l):
+  if l==[] or len(l) == 1:
+    return(True)
+  else:
+    return(...)
+'''
+
+
+def decreasing(l):
+  if l==[] or len(l) == 1:
+    return(True)
+  else:
+    return(
+       # Complete the recursive call below this line
+        decreasing(l[1:]) if l[0]>l[1] else False
+               # Complete the recursive call above this line
+    )
+
+print(decreasing([11,8,7]))
+print(decreasing([3,18,4]))
