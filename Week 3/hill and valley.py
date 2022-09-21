95ab4f1f84e3b92d07a7645b32e33fbb18bb1432
Shreyash Somvanshi <91454227+ShreyashSomvanshi@users.noreply.github.com>
2022-09-21 23:52:05 +0530

    Create hill and valley.py

---
+++ Week 3/hill and valley.py
@@ -0,0 +1,29 @@
+'''
+In a list of integers l, the neighbours of l[i] are l[i-1] and l[i+1]. l[i] is a hill if it is strictly greater than its neighbours and a valley if it is strictly 
+less than its neighbours.
+Write a function counthv(l) that takes as input a list of integers l and returns a list [hc,vc] where hc is the number of hills in l and vc is the number of valleys in l.
+
+Here are some examples to show how your function should work.
+>>> counthv([1,2,1,2,3,2,1])
+[2, 1]
+
+>>> counthv([1,2,3,1])
+[1, 0]
+
+>>> counthv([3,1,2,3])
+[0, 1]
+'''
+
+# Solution:
+
+def counthv(l):
+  hc=0
+  vc=0
+  if len(l)>2:
+    for i in range(1,len(l)-1):
+      if l[i]>l[i-1] and l[i]>l[i+1]:
+        hc+=1
+      elif l[i]<l[i-1] and l[i]<l[i+1]:
+        vc+=1
+  return [hc,vc]
+
